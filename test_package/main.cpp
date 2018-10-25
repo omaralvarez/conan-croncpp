f7f86794136f6434fecb100951d6953e91a10f8c
Omar Alvarez <omar.alvarez@cinfo.es>
2018-10-25 09:42:45 +0200

    Fix warnings and tune example

--- test_package/main.cpp
+++ test_package/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 #include <croncpp/croncpp.h>
 
@@ -8,10 +10,17 @@ int main()
     try
     {
 
-        auto cron = cron::make_cron<cron::cron_standard_traits>("*/5 * * * * *");
+        auto cron = cron::make_cron("*/5 * * * * *");
         
         std::time_t now = std::time(0);
-        std::time_t next = cron::cron_next(cron, now);   
+        std::time_t next = cron::cron_next(cron, now);
+
+        std::cout << next << std::endl;
+
+        std::this_thread::sleep_for(std::chrono::seconds(3));   
+        
+        now = std::time(0);
+        next = cron::cron_next(cron, now);
 
         std::cout << next << std::endl;
 
